9f61f57659751836a1d4d9806ffc8a221e027ec0
boucanier <chironjules.78@gmail.com>
2024-02-09 11:02:12 +0100

    Platform detection

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-import drawDigits, os
+import drawDigits, os, platform
 
 if __name__ == "__main__" :
-    os.system("rm output/*.png")
+    if platform.system() == "Windows" :
+        os.system("del output\\*.png")
+    else :
+        os.system("rm output/*.png")
+        
     listDigitsImg = list()
 
     for i in range(10) :
