e7297c7109d04b9ec2d6e8e387d1d26a7c796bc3
Boucanier <chironjules.78@gmail.com>
2025-01-08 12:23:47 -0500

    Create output dir

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import drawDigits, os, platform
 
 if __name__ == "__main__" :
+    if not os.path.exists("output") :
+        os.mkdir("output")
     if platform.system() == "Windows" :
         os.system("del output\\*.png")
     else :
         os.system("rm output/*.png")
-        
-    listDigitsImg = list()
 
     for i in range(10) :
         drawDigits.generateImg(i, 10)
