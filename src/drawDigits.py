b9c66625d75d5a6c6100f2ea8bcfa4d72f572013
Boucanier <chironjules.78@gmail.com>
2025-01-08 11:46:59 -0500

    Integer division

--- src/drawDigits.py
+++ src/drawDigits.py
@@ -23,66 +23,74 @@ def drawDigit(digit: int, img: ImageDraw.ImageDraw) -> None:
     if digit == 0 :
         bottomToTop = random.choice(list(range(30,100,5)))
         img.ellipse((L+bottomToTop, T, R-bottomToTop, B), fill="white", outline="black", width=random.randint(3,6))
+
     elif digit == 1 :
         offSetMainBarBot = random.choice(list(range(-1,2)))
-        img.line((W * 18/30, T, W * (18+offSetMainBarBot)/30, B), fill = "black", width = random.randint(3, 6))
+        img.line((W * 18//30, T, W * (18+offSetMainBarBot)/30, B), fill = "black", width = random.randint(3, 6))
         topBarLen = random.choice(list(range(100,200,20)))
         offSetTopBar = random.choice(list(range(50,100,10)))
-        img.line((W * 18/30, T, W * 18/30 - topBarLen, T + offSetTopBar), fill = "black", width = random.randint(3, 6))
+        img.line((W * 18//30, T, W * 18//30 - topBarLen, T + offSetTopBar), fill = "black", width = random.randint(3, 6))
         withBottomBar = random.choice([True, False])
         if withBottomBar:
             bottomBarLen = random.choice(list(range(70, 150, 10)))
             offSetBottomBar = random.choice(list(range(-10, 10, 5)))
-            img.line((W * (18+offSetMainBarBot)/30 - bottomBarLen//2, B + offSetBottomBar, W * (18+offSetMainBarBot)/30 + bottomBarLen//2, B - offSetBottomBar), fill = "black", width = random.randint(3, 6))
+            img.line((W * (18+offSetMainBarBot)//30 - bottomBarLen//2, B + offSetBottomBar, W * (18+offSetMainBarBot)//30 + bottomBarLen//2, B - offSetBottomBar), fill = "black", width = random.randint(3, 6))
+    
     elif digit == 2 :
         img.line((L, T, R, T), fill = "black", width = random.randint(3, 6))
-        img.line((R, T, R, H * 5/10), fill = "black", width = random.randint(3, 6))
-        img.line((L, H * 5/10, R, H * 5/10), fill = "black", width = random.randint(3, 6))
-        img.line((L, H * 5/10, L, B), fill = "black", width = random.randint(3, 6))
+        img.line((R, T, R, H * 5//10), fill = "black", width = random.randint(3, 6))
+        img.line((L, H * 5//10, R, H * 5//10), fill = "black", width = random.randint(3, 6))
+        img.line((L, H * 5//10, L, B), fill = "black", width = random.randint(3, 6))
         img.line((L, B, R, B), fill = "black", width = random.randint(3, 6))
+
     elif digit == 3 :
         img.line((L, T, R, T), fill = "black", width = random.randint(3, 6))
         img.line((R, T, R, B), fill = "black", width = random.randint(3, 6))
-        img.line((L, H * 5/10, R, H * 5/10), fill = "black", width = random.randint(3, 6))
+        img.line((L, H * 5//10, R, H * 5//10), fill = "black", width = random.randint(3, 6))
         img.line((L, B, R, B), fill = "black", width = random.randint(3, 6))
+
     elif digit == 4 :
-        img.line((L, T, L, H * 5/10), fill = "black", width = random.randint(3, 6))
-        img.line((L, H * 5/10, R, H * 5/10), fill = "black", width = random.randint(3, 6))
+        img.line((L, T, L, H * 5//10), fill = "black", width = random.randint(3, 6))
+        img.line((L, H * 5//10, R, H * 5//10), fill = "black", width = random.randint(3, 6))
         img.line((R, T, R, B), fill = "black", width = random.randint(3, 6))
+
     elif digit == 5 :
         img.line((L, T, R, T), fill = "black", width = random.randint(3, 6))
-        img.line((L, T, L, H * 5/10), fill = "black", width = random.randint(3, 6))
-        img.line((L, H * 5/10, R, H * 5/10), fill = "black", width = random.randint(3, 6))
-        img.line((R, H * 5/10, R, B), fill = "black", width = random.randint(3, 6))
+        img.line((L, T, L, H * 5//10), fill = "black", width = random.randint(3, 6))
+        img.line((L, H * 5//10, R, H * 5//10), fill = "black", width = random.randint(3, 6))
+        img.line((R, H * 5//10, R, B), fill = "black", width = random.randint(3, 6))
         img.line((L, B, R, B), fill = "black", width = random.randint(3, 6))
+
     elif digit == 6 :
         img.line((L, T, R, T), fill = "black", width = random.randint(3, 6))
         img.line((L, T, L, B), fill = "black", width = random.randint(3, 6))
-        img.line((L, H * 5/10, R, H * 5/10), fill = "black", width = random.randint(3, 6))
-        img.line((R, H * 5/10, R, B), fill = "black", width = random.randint(3, 6))
+        img.line((L, H * 5//10, R, H * 5//10), fill = "black", width = random.randint(3, 6))
+        img.line((R, H * 5//10, R, B), fill = "black", width = random.randint(3, 6))
         img.line((L, B, R, B), fill = "black", width = random.randint(3, 6))
+
     elif digit == 7 :
-        offSetRightBottom = random.randint(0, W * 5/10)
-        offSetLeftTop = random.randint(-H * 1/10, H * 1/10)
-        offSetMid = random.randint(-W * 3/100, W * 3/100)
+        offSetRightBottom = random.randint(0, W * 5//10)
+        offSetLeftTop = random.randint(-H * 1//10, H * 1//10)
+        offSetMid = random.randint(-W * 3//100, W * 3//100)
 
         img.line((W * 3/10, T + offSetLeftTop, R, T), fill = "black", width = random.randint(3, 6))
         img.line((R, T, R - offSetRightBottom, B), fill = "black", width = random.randint(3, 6))
         withBar = random.choice([True, True, True, False])
 
         if withBar :
-            img.line((W * 5/10 - offSetRightBottom / 3, H * 5/10 - offSetMid, R + offSetRightBottom / 3, H * 5/10 + offSetMid), fill = "black", width = random.randint(3, 6))
+            img.line((W * 5//10 - offSetRightBottom // 3, H * 5//10 - offSetMid, R + offSetRightBottom // 3, H * 5//10 + offSetMid), fill = "black", width = random.randint(3, 6))
 
     elif digit == 8 :
         img.line((L, T, R, T), fill = "black", width = random.randint(3, 6))
         img.line((L, T, L, B), fill = "black", width = random.randint(3, 6))
-        img.line((L, H * 5/10, R, H * 5/10), fill = "black", width = random.randint(3, 6))
+        img.line((L, H * 5//10, R, H * 5//10), fill = "black", width = random.randint(3, 6))
         img.line((R, T, R, B), fill = "black", width = random.randint(3, 6))
         img.line((L, B, R, B), fill = "black", width = random.randint(3, 6))
+
     elif digit == 9 :
         img.line((L, T, R, T), fill = "black", width = random.randint(3, 6))
-        img.line((L, T, L, H * 5/10), fill = "black", width = random.randint(3, 6))
-        img.line((L, H * 5/10, R, H * 5/10), fill = "black", width = random.randint(3, 6))
+        img.line((L, T, L, H * 5//10), fill = "black", width = random.randint(3, 6))
+        img.line((L, H * 5//10, R, H * 5//10), fill = "black", width = random.randint(3, 6))
         img.line((R, T, R, B), fill = "black", width = random.randint(3, 6))
         img.line((L, B, R, B), fill = "black", width = random.randint(3, 6))
 
